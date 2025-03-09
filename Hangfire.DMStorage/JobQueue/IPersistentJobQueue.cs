2cb8b5bfb60e624c4cb1fac057ebc5ecb8165434
finder <2289549+findersky@users.noreply.github.com>
2025-03-10 00:01:38 +0800

    ADD Job Queue interface

---
+++ Hangfire.DMStorage/JobQueue/IPersistentJobQueue.cs
@@ -0,0 +1,13 @@
+using System.Data;
+using System.Threading;
+
+using Hangfire.Storage;
+
+namespace Hangfire.DMStorage.JobQueue
+{
+    public interface IPersistentJobQueue
+    {
+        IFetchedJob Dequeue(string[] queues, CancellationToken cancellationToken);
+        void Enqueue(IDbConnection connection, string queue, string jobId);
+    }
+}
