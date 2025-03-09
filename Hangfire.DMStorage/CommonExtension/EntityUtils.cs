a32a3d64049e65b9947a4c3f2ed3344eba36ecf3
finder <2289549+findersky@users.noreply.github.com>
2025-03-09 23:37:37 +0800

    ADD EntityUtils

---
+++ Hangfire.DMStorage/CommonExtension/EntityUtils.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Hangfire.DMStorage.CommonExtension
+{
+    public static class EntityUtils
+    {
+        public static long GetNextId(this IDbConnection connection)
+        {
+            return connection.QuerySingle<long>("SELECT SEQUENCED.NEXTVAL FROM dual");
+        }
+
+        public static long GetNextJobId(this IDbConnection connection)
+        {
+            return connection.QuerySingle<long>("SELECT JOB_ID_SEQ.NEXTVAL FROM dual");
+        }
+    }
+}
