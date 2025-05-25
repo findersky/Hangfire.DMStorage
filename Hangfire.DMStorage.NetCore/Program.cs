e98c539b7c6fe63ebe61c8470bde1174553b3cc2
finder <2289549+findersky@users.noreply.github.com>
2025-05-25 12:35:10 +0800

    Format file content

--- Hangfire.DMStorage.NetCore/Program.cs
+++ Hangfire.DMStorage.NetCore/Program.cs
@@ -19,68 +19,66 @@ namespace Hangfire.DMStorage.NetCore
         static async Task Main(string[] args)
         {
             var host = new HostBuilder()
-    .ConfigureLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Information))
-    .ConfigureServices((hostContext, services) =>
-    {
-        services.Configure<HostOptions>(option =>
-        {
-            option.ShutdownTimeout = TimeSpan.FromSeconds(60);
-        });
+                .ConfigureLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Information))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<HostOptions>(option =>
+                    {
+                        option.ShutdownTimeout = TimeSpan.FromSeconds(60);
+                    });
+                    services.TryAddSingleton<DMStorageOptions>(new DMStorageOptions
+                    {
+                        QueuePollInterval = TimeSpan.FromTicks(1)
+                    });
 
-        services.TryAddSingleton<DMStorageOptions>(new DMStorageOptions
-        {
-            QueuePollInterval = TimeSpan.FromTicks(1)
-        });
+                    services.TryAddSingleton<IBackgroundJobFactory>(x => new CustomBackgroundJobFactory(
+                        new BackgroundJobFactory(x.GetRequiredService<IJobFilterProvider>())));
 
-        services.TryAddSingleton<IBackgroundJobFactory>(x => new CustomBackgroundJobFactory(
-            new BackgroundJobFactory(x.GetRequiredService<IJobFilterProvider>())));
+                    services.TryAddSingleton<IBackgroundJobPerformer>(x => new CustomBackgroundJobPerformer(
+                        new BackgroundJobPerformer(
+                            x.GetRequiredService<IJobFilterProvider>(),
+                            x.GetRequiredService<JobActivator>(),
+                            TaskScheduler.Default)));
 
-        services.TryAddSingleton<IBackgroundJobPerformer>(x => new CustomBackgroundJobPerformer(
-            new BackgroundJobPerformer(
-                x.GetRequiredService<IJobFilterProvider>(),
-                x.GetRequiredService<JobActivator>(),
-                TaskScheduler.Default)));
+                    services.TryAddSingleton<IBackgroundJobStateChanger>(x => new CustomBackgroundJobStateChanger(
+                            new BackgroundJobStateChanger(x.GetRequiredService<IJobFilterProvider>())));
 
-        services.TryAddSingleton<IBackgroundJobStateChanger>(x => new CustomBackgroundJobStateChanger(
-                new BackgroundJobStateChanger(x.GetRequiredService<IJobFilterProvider>())));
+                    services.AddHangfire((provider, configuration) => configuration
+                        .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                        .UseSimpleAssemblyNameTypeSerializer()
+                        .UseStorage(new DMStorage(@"Server=localhost;User Id=SYSDBA;PWD=Chang@2025;DATABASE=DAMENG", new DMStorageOptions
+                        {
+                            TransactionIsolationLevel = IsolationLevel.ReadCommitted,
+                            QueuePollInterval = TimeSpan.FromSeconds(15),
+                            JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                            CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                            PrepareSchemaIfNecessary = true,
+                            DashboardJobListLimit = 50000,
+                            TransactionTimeout = TimeSpan.FromMinutes(1),
+                            SchemaName = "SYSDBA"
+                        })));
 
-        services.AddHangfire((provider, configuration) => configuration
-            .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-            .UseSimpleAssemblyNameTypeSerializer()
-            .UseStorage(new DMStorage(@"Server=localhost;User Id=SYSDBA;PWD=Chang@2025;DATABASE=DAMENG", new DMStorageOptions
-            {
-                TransactionIsolationLevel = IsolationLevel.ReadCommitted,
-                QueuePollInterval = TimeSpan.FromSeconds(15),
-                JobExpirationCheckInterval = TimeSpan.FromHours(1),
-                CountersAggregateInterval = TimeSpan.FromMinutes(5),
-                PrepareSchemaIfNecessary = true,
-                DashboardJobListLimit = 50000,
-                TransactionTimeout = TimeSpan.FromMinutes(1),
-                SchemaName = "SYSDBA"
-            })));
-
-        services.AddHostedService<RecurringJobsService>();
-        services.AddHangfireServer(options =>
-        {
-            options.StopTimeout = TimeSpan.FromSeconds(15);
-            options.ShutdownTimeout = TimeSpan.FromSeconds(30);
-        });
+                    services.AddHostedService<RecurringJobsService>();
+                    services.AddHangfireServer(options =>
+                    {
+                        options.StopTimeout = TimeSpan.FromSeconds(15);
+                        options.ShutdownTimeout = TimeSpan.FromSeconds(30);
+                    });
 
 
-    }).ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.Configure(app =>
-        {
-            // 启用 Hangfire 控制面板
-            app.UseHangfireDashboard("/hangfire", new DashboardOptions
-            {
-                Authorization = new[] { new DashboardAuthorizationFilter() } // 如果需要认证，请自定义认证逻辑
-            });
+                }).ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.Configure(app =>
+                    {
+                        // 启用 Hangfire 控制面板
+                        app.UseHangfireDashboard("/hangfire", new DashboardOptions
+                        {
+                            Authorization = new[] { new DashboardAuthorizationFilter() } // 如果需要认证，请自定义认证逻辑
+                        });
 
-        });
-    })
+                    });
+                })
     .Build();
-
 
             await host.RunAsync();
         }
