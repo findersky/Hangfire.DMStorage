6c8552144e0784214927dc49d2e758a3f0f87852
finder <2289549+findersky@users.noreply.github.com>
2025-05-26 09:37:23 +0800

    Modify the initialization script processing logic

--- src/Hangfire.DMStorage/DMObjectsInstaller.cs
+++ src/Hangfire.DMStorage/DMObjectsInstaller.cs
@@ -21,38 +21,12 @@ namespace Hangfire.DMStorage
                 var userMatch=Regex.Match(connection.ConnectionString, @"(?<=user\s*?id=).+?(?=;)",RegexOptions.IgnoreCase);
                 schemaName = userMatch.Success ? userMatch.Value : "";
             }
-            if (TablesExists(connection, schemaName))
-            {
-                Log.Info("DB tables already exist. Exit install");
-                return;
-            }
             Log.Info("Start installing Hangfire SQL objects...");
             var script = GetStringResource("Hangfire.DMStorage.Install.sql");
-            script =!string.IsNullOrWhiteSpace(schemaName)?script.Replace("[SchemaName]",$"{schemaName}."): script.Replace("[SchemaName]","");
-            var sqlCommands = script.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            script =!string.IsNullOrWhiteSpace(schemaName)?script.Replace("[SchemaNameOnly]", $"{schemaName}").Replace("[SchemaName]",$"{schemaName}."): script.Replace("[SchemaNameOnly]", "").Replace("[SchemaName]","");
+            var sqlCommands = script.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
             sqlCommands.ToList().ForEach(s => connection.Execute(s));
             Log.Info("Hangfire SQL objects installed.");
-        }
-
-        private static bool TablesExists(IDbConnection connection, string schemaName)
-        {
-            string tableExistsQuery;
-
-            if (!string.IsNullOrWhiteSpace(schemaName))
-            {
-                tableExistsQuery = $@"SELECT TABLE_NAME
-FROM all_tables
-WHERE OWNER = '{schemaName}' AND  TABLE_NAME IN('AggregatedCounter','Counter','DistributedLock','Hash','Job','JobParameter','JobQueue','JobState','List','Server','Set','State')
-ORDER BY TABLE_NAME;";
-            }
-            else
-            {
-                tableExistsQuery = @"SELECT TABLE_NAME
-FROM all_tables
-WHERE TABLE_NAME IN('AggregatedCounter','Counter','DistributedLock','Hash','Job','JobParameter','JobQueue','JobState','List','Server','Set','State')
-ORDER BY TABLE_NAME;";
-            }
-            return connection.ExecuteScalar<string>(tableExistsQuery) != null;
         }
 
         private static string GetStringResource(string resourceName)
